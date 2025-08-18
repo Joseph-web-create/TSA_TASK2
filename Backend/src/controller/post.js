af814d8ce98a4570874b8121a98164fc2fbb67eb
Aace <aacejovi@gmail.com>
2025-08-18 11:20:27 +0100

    Getting the user id

--- Backend/src/controller/post.js
+++ Backend/src/controller/post.js
@@ -4,14 +4,12 @@ import Post from "../models/post.js";
 export const createPost = async (req, res, next) => {
   const { title, content } = req.body;
 
-  const author = req.user._id;
+  const author = req.user.id;
   const user = req.user;
 
   try {
     if (!title || !content) {
-      return next(
-        createHttpError(400, "Title, content, and author are required")
-      );
+      return next(createHttpError(400, "Title and content are required"));
     }
 
     const post = await Post.create({
@@ -66,7 +64,7 @@ export const updatePost = async (req, res, next) => {
 
     if (!updatedPost) return next(createHttpError(404, "Post not found"));
 
-    if (req.user.role !== "admin" && updatePost.author !== req.user._id) {
+    if (req.user.role !== "admin" && updatePost.author !== req.user.id) {
       return next(
         createHttpError(403, "Forbidden: you can only update you own post")
       );
@@ -90,7 +88,7 @@ export const deletePost = async (req, res, next) => {
 
     if (!deletedPost) return next(createHttpError(404, "Post not found"));
 
-    if (req.user.role !== "admin" && updatePost.author !== req.user._id) {
+    if (req.user.role !== "admin" && updatePost.author !== req.user.id) {
       return next(
         createHttpError(403, "Forbidden: you can only delete you own post")
       );
